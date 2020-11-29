4db8afc633756407adabefd2edbe5975d4c1d80e
Lydia Feng <lydia.x.feng@gmail.com>
2020-11-28 20:19:08 -0500

    added struct

--- elevator_part_2.c
+++ elevator_part_2.c
@@ -11,25 +11,29 @@
 #include <pthread.h>
 #include "elevator.h"
 
-Dllist people_list;
-pthread_cond_t *holding;
-pthread_cond_t *arrived;
-pthread_mutex_t *global_lock;
+typedef struct {
+    Dllist people_list;
+    pthread_cond_t *holding;
+    pthread_cond_t *arrived;
+    pthread_mutex_t *global_lock;
+}vpointer;
+
 
 void initialize_simulation(Elevator_Simulation *es)
 {
     /* Called once at the beginning of the simulation */
-    people_list = new_dllist();
-    holding = (pthread_cond_t *) malloc(sizeof(pthread_cond_t));
-    pthread_cond_init(holding, NULL);
+    vpointer *vpointer1 = (vpointer *)malloc(sizeof(vpointer));
+    vpointer1->people_list = new_dllist();
+    vpointer1->holding = (pthread_cond_t *) malloc(sizeof(pthread_cond_t));
+    pthread_cond_init(vpointer1->holding, NULL);
 
-    arrived = (pthread_cond_t *) malloc(sizeof(pthread_cond_t));
-    pthread_cond_init(arrived, NULL);
+    vpointer1->arrived = (pthread_cond_t *) malloc(sizeof(pthread_cond_t));
+    pthread_cond_init(vpointer1->arrived, NULL);
 
-    global_lock = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
-    pthread_mutex_init(global_lock, NULL);
+    vpointer1->global_lock = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init(vpointer1->global_lock, NULL);
 
-    es->v = people_list;    //void pointer to list of people
+    es->v = vpointer1;    //void pointer to list of people
 
 }
 
@@ -43,12 +47,12 @@ void initialize_person(Person *e)
 
 void wait_for_elevator(Person *p)
 {
-    Dllist *position;
-    position = (Dllist *)(p->es->v);
+    vpointer *position;
+    position = (vpointer *)(p->es->v);
 
     pthread_mutex_lock(p->es->lock);
-    dll_append(people_list, new_jval_v(p));
-    pthread_cond_signal(holding);
+    dll_append(position->people_list, new_jval_v(p));
+    pthread_cond_signal(position->holding);
     pthread_mutex_unlock(p->es->lock);
 
     pthread_mutex_lock(p->lock);
@@ -78,10 +82,10 @@ void person_done(Person *p)
     pthread_mutex_unlock(el->lock);
 }
 
-void check_for_people_to_unload(Elevator * el, Dllist to_unload) {
+void check_for_people_to_unload(Elevator * el, Dllist to_unload, vpointer * el_vpointer) {
     Dllist temp = el->people->flink;
     //iterate through people on the elevator
-    while (temp != to_unload && to_unload != NULL) {
+    while (temp != el->people && !dll_empty(el->people)) {
         Person *p = (Person *) (temp->val.v);
 
         //if person is getting off, signal person
@@ -101,21 +105,29 @@ void check_for_people_to_unload(Elevator * el, Dllist to_unload) {
     }
 }
 
-void check_for_people_to_load(Elevator * el, Dllist to_unload, int direction) {
-    pthread_mutex_lock(global_lock);
-    Dllist temp = people_list->flink;
+void check_for_people_to_load(Elevator * el, Dllist to_unload, int direction, vpointer * el_vpointer) {
+    printf("checking to load\n");
+    pthread_mutex_lock(el_vpointer->global_lock);
+    printf("locked\n");
+    Dllist temp = el_vpointer->people_list->flink;
+    //Dllist temp = dll_first(el_vpointer->people_list);
     //while (!dll_empty(temp) || temp != NULL) {
-    while (temp != people_list && people_list != NULL) {
+    printf("1");
+    while (temp != el_vpointer->people_list && !dll_empty(el_vpointer->people_list)) {
+        printf("2");
         Person * p = (Person *)(temp->val.v);
+        Dllist temp2 = temp->flink;
 
         if (  (el->onfloor == p->from) && (((p->to > el->onfloor) && (direction == 1)) || ((p->to < el->onfloor) && (direction == 0)))) {
             dll_append(to_unload, temp->val);
+
+            dll_delete_node(temp);
         }
 
-        temp = temp->flink;
+        temp = temp2;
     }
 
-    pthread_mutex_unlock(global_lock);
+    pthread_mutex_unlock(el_vpointer->global_lock);
 }
 
 void *elevator(void *arg)
@@ -124,14 +136,16 @@ void *elevator(void *arg)
     Elevator *el = (Elevator *) arg;
     Person *p;
     int direction = 1;
-    Dllist to_unload;
+    Dllist to_unload = NULL;
+    vpointer *el_vpointer = (vpointer *)(el->es->v);
 
     while (1) {
 
         //if no people in simulation yet
         pthread_mutex_lock(el->es->lock);
-        while(people_list == NULL) {
-            pthread_cond_wait(holding, el->es->lock);
+        while(dll_empty(el_vpointer->people_list)) {
+            printf("no one here yet\n");
+            pthread_cond_wait(el_vpointer->holding, el->es->lock);
         }
         pthread_mutex_unlock(el->es->lock);
 
@@ -140,28 +154,32 @@ void *elevator(void *arg)
         } else if (el->onfloor == el->es->nfloors) {
             direction = 0;
         }
+        printf("sending elevators the right direction\n");
 
         //LF
-        if (!dll_empty(people_list)) {
-            check_for_people_to_load(el, to_unload, direction);
 
-            Dllist temp = to_unload->flink;
-            while (temp->flink != NULL) {
-                p = (Person *) temp->val.v;
-                if (el->door_open == 0) {
-                    open_door(el);
-                }
+        check_for_people_to_load(el, to_unload, direction, el_vpointer);
 
-                pthread_mutex_lock(p->lock);
-                p->e = el;
-                pthread_cond_signal(p->cond);
-                pthread_mutex_lock(el->lock);
-                pthread_mutex_unlock(p->lock);
-
-                pthread_cond_wait(el->cond, el->lock);
-                pthread_mutex_unlock(el->lock);
-                temp = temp->flink;
+        Dllist temp = to_unload->flink;
+        while (temp != to_unload && !dll_empty(to_unload)) {
+            p = (Person *) temp->val.v;
+            if (el->door_open == 0) {
+                open_door(el);
             }
+
+            pthread_mutex_lock(p->lock);
+            p->e = el;
+            pthread_cond_signal(p->cond);
+            pthread_mutex_lock(el->lock);
+            pthread_mutex_unlock(p->lock);
+
+            pthread_cond_wait(el->cond, el->lock);
+            pthread_mutex_unlock(el->lock);
+
+            Dllist temp2 = temp->flink;
+            dll_delete_node(temp);
+            temp = temp2;
+
         }
 
         if (el->door_open == 1) {
@@ -175,7 +193,7 @@ void *elevator(void *arg)
         }
 
         //LF
-        check_for_people_to_unload(el, to_unload);
+        check_for_people_to_unload(el, to_unload, el_vpointer);
 
 
     }
